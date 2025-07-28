6db71dead24b8f12b8b76ea8be88746513406849
mtani <mtani1991@gmail.com>
2025-07-28 17:40:07 +0300

    Update HelloWorld.java

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("Hello There Mtani!!!");//from c1 and demo1 branch
+		System.out.print("Hello There Mtani!!!");//from c1 branch
 	}
 
 }
