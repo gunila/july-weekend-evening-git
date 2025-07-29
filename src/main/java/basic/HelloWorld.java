55e59420a577a5ca8caa614669de7ec352a7b449
mtani <mtani1991@gmail.com>
2025-07-29 17:54:42 +0300

    Update HelloWorld.java

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -7,5 +7,13 @@ public class HelloWorld {
 
 		System.out.print("Hello There Mtani!!!");//from c1 branch and a more to come
 	}
+	
+	public void showMessage() {
+		System.out.print("New message here!");//from c1 branch and a more to come
+	}
+	
+	public void showOldMessage() {
+		System.out.print("Old message here!");//from c1 branch and a more to come
+	}
 
 }
